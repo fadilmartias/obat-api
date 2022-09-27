14a3f12c3226f42683011d4b49137ca578b45303
fadilmartias <fadilmartias26@gmail.com>
2022-09-27 09:53:38 +0700

    fix router

--- routes/web.php
+++ routes/web.php
@@ -19,23 +19,19 @@ $router->get('/', function () use ($router) {
 });
 
 //AUTH
-Route::group([
-
-    'prefix' => 'api'
-
-], function ($router) {
-    Route::post('login', 'AuthController@login');
-    Route::post('logout', 'AuthController@logout');
-    Route::post('refresh', 'AuthController@refresh');
-    Route::post('user-profile', 'AuthController@me');
+Route::group(['prefix' => 'api'], function ($router) {
+    $router->post('login', 'AuthController@login');
+    $router->post('logout', 'AuthController@logout');
+    $router->post('refresh', 'AuthController@refresh');
+    $router->post('user-profile', 'AuthController@me');
 
 });
 
 //CRUD Obat
 Route::group(['middleware' => 'auth'], function($router) {
-$router->get('obat', 'ObatController@index');
-$router->post('/obat', 'ObatController@store');
-$router->get('/obat/{id}', 'ObatController@show');
-$router->put('/obat/{id}', 'ObatController@update');
-$router->delete('/obat/{id}', 'ObatController@destroy');
+    $router->get('obat', 'ObatController@index');
+    $router->post('/obat', 'ObatController@store');
+    $router->get('/obat/{id}', 'ObatController@show');
+    $router->put('/obat/{id}', 'ObatController@update');
+    $router->delete('/obat/{id}', 'ObatController@destroy');
 });
